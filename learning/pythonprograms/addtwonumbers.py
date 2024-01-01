321aa28b29dcd72d031a0e4232d618fab2bda60a
Anju Mohan <amohan@anjus-mbp.mynetworksettings.com>
2024-01-01 12:51:57 -0500

    add two numbers

--- learning/pythonprograms/addtwonumbers.py
+++ learning/pythonprograms/addtwonumbers.py
@@ -1,5 +1,13 @@
+
 def add(a,b):
     return a+b
+
+import operator
+
+def add2(a,b):
+    return operator.add(a,b)
+
+add3 = lambda x,y: x+y
 
 if __name__== '__main__':
 
@@ -7,7 +15,9 @@ if __name__== '__main__':
 
         a = int(input('Enter first number : '))
         b = int(input('Enter second number : '))
-        print('Sum of {} + {} =  {} '.format(a, b, add(a,b)) )
+        print('Sum(simple) of {} + {} =  {} '.format(a, b, add(a,b)) )
+        print('Sum(using operator) of {} + {} =  {} '.format(a, b, add2(a,b)) )
+        print('Sum(using lambda) of {} + {} =  {} '.format(a, b, add3(a,b)) )
 
     except ValueError:
         print('invalid value given in the input')
